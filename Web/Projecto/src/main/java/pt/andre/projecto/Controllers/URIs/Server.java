a15297b251fa4a01d8e2a4e361574af5049cf7b1
André Carvalho <andre.daniel.carvalho@gmail.com>
2017-09-03 19:03:21 +0100

    [Server] * Add download files

--- Web/Projecto/src/main/java/pt/andre/projecto/Controllers/URIs/Server.java
+++ Web/Projecto/src/main/java/pt/andre/projecto/Controllers/URIs/Server.java
@@ -4,13 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pt.andre.projecto.Model.Utils.Device;
 import pt.andre.projecto.Model.Utils.DeviceIdentifier;
 import pt.andre.projecto.Service.Interfaces.IServerService;
@@ -53,16 +51,6 @@ public class Server{
     public String removeDevices(Map<String, Object> model, @RequestHeader("Authorization") String token, @RequestParam String deviceIdentifier, @RequestParam boolean isMobile){
         return service.deleteDevice(model, token, deviceIdentifier, isMobile);
     }
-
-
-    @RequestMapping(value = "/YO", method = RequestMethod.GET)
-    public String yo(){
-        service.notifyRemovedDevice("103482874633874499718", "97F82594-4CCF-46A6-8D5B-7F8CF5CDF0F0");
-        return "OAL";
-    }
-
-
-
 
 }
 

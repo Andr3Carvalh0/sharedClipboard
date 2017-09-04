36fc24b10609fe5b06f87d7242544cf1f9d87977
André Carvalho <andre.daniel.carvalho@gmail.com>
2017-09-04 20:39:50 +0100

    [Android] * Improvements

--- Android/ProjectoESeminario/app/src/main/java/andre/pt/projectoeseminario/View/Fragments/HistoryFragment.java
+++ Android/ProjectoESeminario/app/src/main/java/andre/pt/projectoeseminario/View/Fragments/HistoryFragment.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import andre.pt.projectoeseminario.View.Adapters.Clipboard.ClipboardCategoriesAdapter;

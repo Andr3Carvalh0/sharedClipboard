7c6932ab503a5ee479c160155d7df363d4f35b45
André Carvalho <andre.daniel.carvalho@gmail.com>
2017-09-12 12:21:12 +0100

    [Android] * Update boot service

--- Android/ProjectoESeminario/app/src/main/java/andre/pt/projectoeseminario/BroadcastReceiver/BootCompletedReceiver.java
+++ Android/ProjectoESeminario/app/src/main/java/andre/pt/projectoeseminario/BroadcastReceiver/BootCompletedReceiver.java
@@ -5,17 +5,21 @@ import android.content.Context;
 import android.content.Intent;
 import andre.pt.projectoeseminario.Controller.Preferences;
 import andre.pt.projectoeseminario.Services.CopyMenuListener;
+import andre.pt.projectoeseminario.Services.FirebaseMessageHandler;
 
 
 /**
  * Broadcast that start the listening service when the device boots up
 */
-public class AutoStart extends BroadcastReceiver {
+public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if(new Preferences(context).getBooleanPreference(Preferences.SERVICERUNNING)) {
             Intent i = new Intent(context, CopyMenuListener.class);
+            context.startService(i);
+
+            i = new Intent(context, FirebaseMessageHandler.class);
             context.startService(i);
         }
     }
